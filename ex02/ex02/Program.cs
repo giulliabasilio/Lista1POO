eaac102986a3d3451740ec805b079a983893144d
giulliabasilio <giulliabasilio@gmail.com>
2025-09-08 16:34:57 -0300

    Add files via upload

---
+++ ex02/ex02/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrado quadradao = new Quadrado();
+
+            Console.WriteLine("Digite o valor da aresta do quadrado:");
+
+            quadradao.setLado(int.Parse(Console.ReadLine()));
+
+            quadradao.calcularArea();
+
+            Console.WriteLine(quadradao.getArea());
+
+
+        }
+    }
+}
---
+++ ex02/ex02/Quadrado.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex02
+{
+    internal class Quadrado
+    {
+     
+        
+            int l1;
+            int a;
+
+            public Quadrado()
+            {
+                this.l1 = 0;
+                this.a = 0;
+            }
+
+
+
+            public void setLado(int l1)
+            {
+                this.l1 = l1;
+            }
+
+
+
+
+            public int getLado()
+            {
+                return this.l1;
+            }
+
+
+
+            public int getArea()
+            {
+                return this.a;
+            }
+
+            public void calcularArea()
+            {
+                this.a = this.l1 * 2;
+            }
+
+
+        }
+    }
+
+
+     
+
+
