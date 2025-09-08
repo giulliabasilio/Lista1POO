cd37a58a0077a96fab0e8d4f0c57b207299a2568
giulliabasilio <giulliabasilio@gmail.com>
2025-09-08 16:39:35 -0300

    Add files via upload

---
+++ ex03/ex03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Quadradodiag becoDiagonal = new Quadradodiag();
+
+
+            Console.WriteLine("Digite o valor da diagonal do quadrado");
+            becoDiagonal.setDiag(double.Parse(Console.ReadLine()));
+
+            becoDiagonal.calculaArea();
+
+            Console.WriteLine("O valor da área do quadrado é: {0}", becoDiagonal.getArea());
+        }
+    }
+}
---
+++ ex03/ex03/Quadradodiag.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex03
+{
+    internal class Quadradodiag
+    {
+        double diag;
+        double a;
+
+        public Quadradodiag()
+        {
+            this.diag = 0;
+            this.a = 0;
+
+        }
+
+        public void setDiag(double diag)
+        {
+
+
+            this.diag = diag;
+
+        }
+
+        public double getDiag()
+        {
+
+            return this.diag;
+
+        }
+
+        public double getArea()
+        {
+
+            return this.a;
+
+        }
+
+        public void calculaArea()
+        {
+
+            this.a = Math.Pow(this.diag, 2) / 2;
+
+
+        }
+    }
+}
+    
