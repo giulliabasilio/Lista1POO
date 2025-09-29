843b5fc67f00d4f9b58e178cf7e596062165b893
giulliabasilio <giulliabasilio@gmail.com>
2025-09-29 19:26:13 -0300

    Add files via upload

---
+++ ex07/ex07/Maior.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex07
+{
+    internal class Maior
+    {
+        double valorA;
+        double valorB;
+
+
+        public Maior()
+        {
+            this.valorA = 0;
+            this.valorB = 0;
+
+
+        }
+
+        public void setvalorA(double valorA)
+        {
+
+
+            this.valorA = valorA;
+
+        }
+
+        public void setvalorB(double valorB)
+        {
+
+
+            this.valorB = valorB;
+
+        }
+
+
+        public double verificaMaior()
+        {
+            if (this.valorA > this.valorB)
+            {
+                return this.valorA;
+
+            }
+            else
+            {
+                return this.valorB;
+            }
+        }
+    }
+}
---
+++ ex07/ex07/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Maior thatFace = new Maior();
+
+
+            Console.WriteLine("Digite o valor A: ");
+            thatFace.setvalorA(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite o valor B: ");
+            thatFace.setvalorB(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("O maior valor é: {0} ", thatFace.verificaMaior());
+        }
+    }
+}
